35aa5811865a34a963f9c5cf094295fa2a93f570
Roger Santos <rsfernandes@outlook.com>
2021-01-04 19:10:04 -0300

    Adds finding avarega exercise

---
+++ exercises/basic/data-types/finding-percentage.py
@@ -0,0 +1,31 @@
+# The provided code stub will read in a dictionary containing key/value pairs
+# of name: [marks] for a list of students. Print the average of the marks array
+# for the student name provided, showing 2 places after the decimal.
+
+# Sample Input 0
+# 3
+# Krishna 67 68 69
+# Arjun 70 98 63
+# Malika 52 56 60
+# Malika
+
+# Sample Output 0
+# 56.00
+
+from functools import reduce
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+
+    def sum_all(acc, value): return acc + value
+
+    avarage = reduce(sum_all,
+                     student_marks[query_name]) / 3
+
+    print("{:0.2f}".format(avarage))
