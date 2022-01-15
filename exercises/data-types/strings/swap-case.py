f6d8346d3fbb37e4e73cdc5843897657f53dd4df
Roger Santos <rsfernandes@outlook.com>
2022-01-15 19:39:39 -0300

    Adds swap_case exercise

---
+++ exercises/data-types/strings/swap-case.py
@@ -0,0 +1,13 @@
+def swap_case(s):
+    swapped_s = ''
+
+    for letter in s:
+        swapped_s += letter.lower() if letter.isupper() else letter.upper()
+
+    return swapped_s
+
+
+if __name__ == '__main__':
+    s = raw_input()
+    result = swap_case(s)
+    print result
