ac086a9651f1d105b585ab26faa7bf7d86e1669c
Roger Santos <rsfernandes@outlook.com>
2022-03-25 16:48:25 -0300

    feat: adds flipping bits challenge

---
+++ exercises/javascript/bits/flipping-bits.js
@@ -0,0 +1,13 @@
+/*
+ * Complete the 'flippingBits' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER n as parameter.
+ */
+function flippingBits(n) {
+  /**
+   * using the ~ (NOT) operator, we get the invert 32 bit sequence
+   * and then we apply the fill right shift to add the necessary zeros to the left
+   */
+  return ~n >>> 0;
+}
